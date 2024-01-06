8207cbbb5e86a89c9f8c8bf6149337a66d721f5f
Shitanshukumar607 <147914822+Shitanshukumar607@users.noreply.github.com>
2024-01-06 17:40:07 +0530

    Update whackamole.js

--- whackamole.js
+++ whackamole.js
@@ -1,30 +1,30 @@
 let score = 0;
 let time = 10;
-let y
+let highscore = 0;
+let y;
 let Interval;
-
-function RandomNumber() {
-    ;
-    return Math.floor(Math.random() * 9) + 1
-};
 
 let TimeLeftEl = document.getElementById("timeleft")
 let ScoreEl = document.getElementById("score")
+let HighscoreEl = document.getElementById("highscore")
 
 TimeLeftEl.textContent = `Time : ${time}`
 ScoreEl.textContent = `Your Score : ${score}`
 
+//Checks if highscore variable isn't 0 then it takes the old highscore 
+
+if (JSON.parse(localStorage.getItem("highsc")) == null) {
+    highscore = 0
+} else {
+    highscore = JSON.parse(localStorage.getItem("highsc"))
+}
+
+HighscoreEl.textContent = `Your Highscore : ${highscore}`
+
+
 let Startbtn = document.getElementById("start")
 let Moles = document.querySelectorAll(".mole");
 let Bushes = document.querySelectorAll(".bush");
-
-Moles.forEach(value => {
-    value.setAttribute("draggable", false)
-})
-Bushes.forEach(value => {
-    value.setAttribute("draggable", false)
-})
-
 
 
 Startbtn.addEventListener("click", start)
@@ -51,6 +51,68 @@ function MoveTheMole() {
 
         // adding event Listener to both bush and mole
         document.getElementById(`mole${x}`).addEventListener("click", scoreplus)
+        document.getElementById(`bush${x}`).addEventListener("click", scoreplus);
+    }
+};
+
+
+//this function adds 1 to score 
+function scoreplus() {
+    score++
+    ScoreEl.textContent = `Your Score : ${score}`
+
+    // removing event Listener to both bush and mole
+    Moles.forEach(value => {
+        value.removeEventListener("click", scoreplus)
+    })
+    Bushes.forEach(value => {
+        value.removeEventListener("click", scoreplus)
+    })
+
+    //making mole go into bush
+    document.getElementById(`mole${y}`).style.transform = "scale(0.5)"
+    console.log(score);
+    MoveTheMole()
+}
+
+let TimeLeftfun = () => {
+    if (time > 0) {
+        TimeMinusOne()
+    } else {
+        clearInterval(Interval)
+        Startbtn.addEventListener("click", start)
+        Moles.forEach(value => {
+            value.removeEventListener("click", scoreplus)
+        })
+        Bushes.forEach(value => {
+            value.removeEventListener("click", scoreplus)
+        })
+        document.getElementById(`mole${y}`).style.transform = "scale(0.5)"
+
+        if (score > highscore) {
+            highscore = score
+            HighscoreEl.textContent = `Your Highscore : ${highscore}`
+            localStorage.setItem("highsc", JSON.stringify(highscore))
+        }
+
+        alert(`You Scored ${score} in 10 seconds.`)
+
+        Startbtn.addEventListener("click", start)
+        time = 10
+        TimeLeftEl.textContent = `Time : ${time}`
+    }
+}
+
+//Random number 
+function RandomNumber() {
+    return Math.floor(Math.random() * 9) + 1
+};
+
+//decreases the time by 1
+function TimeMinusOne() {
+    time--
+    TimeLeftEl.textContent = `Time left : ${time}`
+}
         document.getElementById(`bush${x}`).addEventListener("click", scoreplus);
     }
 };
